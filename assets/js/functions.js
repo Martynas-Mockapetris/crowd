a2a2d590c4d1500324f50a0edec30b57060e6be2
Martynas Mockapetris <78859416+Martynas-Mockapetris@users.noreply.github.com>
2024-09-26 16:58:56 +0300

    Add files via upload

---
+++ assets/js/functions.js
@@ -0,0 +1,31 @@
+$(function() {
+	smoothScroll(500);
+
+});
+
+function smoothScroll (duration) {
+	$('a[href^="#"]').on('click', function(event) {
+
+	    var target = $( $(this).attr('href') );
+
+	    if( target.length ) {
+	        event.preventDefault();
+	        $('html, body').animate({
+	            scrollTop: target.offset().top
+	        }, duration);
+	    }
+	});
+}
+
+$(".header").click(function () {
+
+    $header = $(this);
+    //getting the next element
+    $content = $header.next();
+    //open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
+    $content.slideToggle(500, function () {
+        //execute this after slideToggle is done
+        //change text of header based on visibility of content div
+    });
+
+});
